9d9a2473fa2bcf8fbc043f97a3f147f1d5a1289a
Neb <nex.shaw.jobs@gmail.com>
2021-12-06 13:38:03 -0800

    Initial Commit

---
+++ exer1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exer1
+//
+//  Created by NebSha on 12/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
